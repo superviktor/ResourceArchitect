626f677653d0d3e480c595a57dabaf603f9cecf0
Viktor Prykhidko <victor.pryhidko@gmail.com>
2018-09-29 12:30:46 +0300

    get kvp

---
+++ ResourceArchitectSaturdayVersion/KeyValuePair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceArchitectSaturdayVersion
+{
+    public class KeyValuePair
+    {
+        public int LineNumber { get; set; }
+        public string Key { get; set; }
+        public string EngValue { get; set; }
+        public string FraValue { get; set; }
+        public string GerValue { get; set; }
+        public string QueValue { get; set; }
+        public string OriginalResFileName { get; set; }
+    }
+}
--- ResourceArchitectSaturdayVersion/Program.cs
+++ ResourceArchitectSaturdayVersion/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,51 @@ namespace ResourceArchitectSaturdayVersion
     {
         static void Main(string[] args)
         {
+            var origilalResourceFiles = Directory.GetFiles(@"A:\Work\Source\Repos\onyx\Source\FUEL.Web.UI", "*resx", SearchOption.AllDirectories)
+                         .Where(x => !x.Contains(".uk.resx")
+                         && !x.Contains(".de.resx") &&
+                         !x.Contains(".fr-FR.resx") &&
+                         !x.Contains(".fr-CA.resx"))
+                         .ToList();
+
+            var engValues = File.ReadAllLines(@"A:\Work\eng.txt").ToList();
+            var fraValues = File.ReadAllLines(@"A:\Work\fra.txt").ToList();
+            var gerValues = File.ReadAllLines(@"A:\Work\ger.txt").ToList();
+            var queValues = File.ReadAllLines(@"A:\Work\que.txt").ToList();
+
+            var kvps = new List<KeyValuePair>();
+
+            foreach (var engValue in engValues)
+            {
+                var index = engValues.IndexOf(engValue);
+
+                foreach (var resFile in origilalResourceFiles)
+                {
+                    using (var sourceReader = new ResXResourceReader(resFile))
+                    {
+                        foreach (DictionaryEntry entry in sourceReader)
+                        {
+                            var value = entry.Value.ToString();
+                            if (engValue == value)
+                            {
+                                var kvp = new KeyValuePair
+                                {
+                                    Key = entry.Key.ToString(),
+                                    LineNumber = index,
+                                    EngValue = engValue,
+                                    FraValue = fraValues[index],
+                                    GerValue = gerValues[index],
+                                    QueValue = queValues[index],
+                                    OriginalResFileName = resFile
+                                };
+                                kvps.Add(kvp);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine();
         }
     }
 }
